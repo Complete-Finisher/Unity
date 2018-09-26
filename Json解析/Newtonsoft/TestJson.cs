074ae210f69f43b5045b9cd4db94097759fd5c4f
CodeMonkey <123456@123.com>
2018-09-26 16:22:57 +0800

    UI穿透

---
+++ Json解析/Newtonsoft/TestJson.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Newtonsoft.Json;
+
+public class TestJson : MonoBehaviour
+{
+    //Unity自己的json序列化是不支持字典格式的。无意间发现了一个全新的json .net库，功能很强大，还支持序列化字典推荐给大家。
+    //http://www.newtonsoft.com/json
+    //点击下载，zip.解压后，把bin/net20/Newtonsoft.Json.dll 拖入unity工程。
+    //写下一段简单的序列化 反序列化json的代码
+    void Start()
+    {
+
+        Product product = new Product();
+        product.dic["字典key"] = "字典Value";
+        product.name = "我是雨松MOMO";
+        string json = JsonConvert.SerializeObject(product);
+
+        Product m = JsonConvert.DeserializeObject<Product>(json);
+
+        Debug.Log(json);
+        Debug.Log(m.name);
+        Debug.Log(m.dic["字典key"]);
+    }
+
+
+    public class Product
+    {
+        public string name;
+        public Dictionary<string, string> dic = new Dictionary<string, string>();
+    }
+
+}
---
+++ UI穿透/BackUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BackUI : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //监听按下
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("背后UI OnPointerDown");
+    }
+
+    //监听抬起
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("背后UI OnPointerUp");
+    }
+
+    //监听点击
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("背后UI OnPointerClick");
+    }
+
+}
